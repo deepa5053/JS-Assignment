1ef1ff2d8ba91f3d030de91d3647458b812047a9
Deepa Birari <112015631+deepa5053@users.noreply.github.com>
2023-11-13 15:26:09 +0100

    html css complete

--- js/index.js
+++ js/index.js
@@ -1,14 +1,26 @@
 
+ document.addEventListener('DOMContentLoaded', function() {
+
+    const urlParams = new URLSearchParams(window.location.search);
+    const planetName = urlParams.get('planet');
+  
+
+    document.getElementById('planet-name').innerText = planetName;
+  
+
+    document.getElementById('planet-description').innerText = getPlanetDescription(planetName);
+  });
+  
   
 
   async function getPlanetDescription(planetName) {
-   let resp = await fetch("https://n5n3eiyjb0.execute-api.eu-north-1.amazonaws.com/keys", {
-       method: "POST"
-   });
-  //let resp = await fetch("https://n5n3eiyjb0.execute-api.eu-north-1.amazonaws.com/bodies", {
- // method: "GET",
- //headers: { "x-zocom": "solaris-JAaNDtW2DyvIHS96" },
-//});
+  // let resp = await fetch("https://n5n3eiyjb0.execute-api.eu-north-1.amazonaws.com/keys", {
+    //   method: "POST"
+  // });
+ let resp = await fetch("https://n5n3eiyjb0.execute-api.eu-north-1.amazonaws.com/bodies", {
+ method: "GET",
+ headers: { "x-zocom": "solaris-JAaNDtW2DyvIHS96" },
+});
   
     let temp = await resp.json();
     let bodies= temp.bodies;
@@ -21,3 +33,13 @@
        console.log(bodies[i].latinName);
     }
   }
+
+
+  const planets = document.querySelectorAll('.planet');
+
+  planets.forEach(planet => {
+    planet.addEventListener('click', function() {
+      const planetName = this.classList[1]; // Assumes the second class is the planet name
+      window.location.href = `planet-details.html?planet=${planetName}`;
+    });
+  });
