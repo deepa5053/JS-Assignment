2d48edf0b0e1e6f83219ecc3a6db18bafd346b15
Deepa Birari <112015631+deepa5053@users.noreply.github.com>
2023-11-13 14:38:32 +0100

    fetch api used by changing key

--- js/index.js
+++ js/index.js
@@ -1,37 +1,23 @@
-document.addEventListener('DOMContentLoaded', function() {
- 
-    const urlParams = new URLSearchParams(window.location.search);
-    const planetName = urlParams.get('planet');
+
   
 
-    document.getElementById('planet-name').innerText = planetName;
+  async function getPlanetDescription(planetName) {
+   let resp = await fetch("https://n5n3eiyjb0.execute-api.eu-north-1.amazonaws.com/keys", {
+       method: "POST"
+   });
+  //let resp = await fetch("https://n5n3eiyjb0.execute-api.eu-north-1.amazonaws.com/bodies", {
+ // method: "GET",
+ //headers: { "x-zocom": "solaris-JAaNDtW2DyvIHS96" },
+//});
   
+    let temp = await resp.json();
+    let bodies= temp.bodies;
+    console.log(bodies);
 
-    document.getElementById('planet-description').innerText = getPlanetDescription(planetName);
-  });
-  
-
-  function getPlanetDescription(planetName) {
-    switch (planetName) {
-      case 'Sun':
-        return 'The Sun is the star at the center of the solar system.';
-      case 'Mercury':
-        return 'Mercury is the smallest planet in the solar system.';
-      case 'Venus':
-        return 'Venus is the second planet from the Sun.';
-      case 'Earth':
-        return 'Earth is our home planet and the only one with known life.';
-      case 'Mars':
-        return 'Mars is known as the Red Planet and is the fourth planet from the Sun.';
-      case 'Jupiter':
-        return 'Jupiter is the largest planet in the solar system.';
-      case 'Saturn':
-        return 'Saturn is known for its beautiful rings.';
-      case 'Uranus':
-        return 'Uranus is a gas giant and the seventh planet from the Sun.';
-      case 'Neptune':
-        return 'Neptune is the eighth and farthest known planet from the Sun.';
-      default:
-        return 'Planet information not available.';
+    // Loop
+    for (let i=0; i<9; i++)
+    {
+       if (bodies[i].name==planetName)
+       console.log(bodies[i].latinName);
     }
   }
