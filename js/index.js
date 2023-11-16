7a2d1594fa999c8850da85e3fdbd271fd1057dbe
Deepa Birari <112015631+deepa5053@users.noreply.github.com>
2023-11-16 23:39:32 +0100

    final complete

--- js/index.js
+++ js/index.js
@@ -17,21 +17,29 @@ async function getPlanetDescription(planetName) {
     console.log(temp);
 
     // Loop
+    let planetFound= false;
     for (let i=0; i<9; i++)
   
      {
        if (bodies[i].name==planetName)
           {
               console.log("Found the planet name");
+              planetFound=true;
               document.getElementById('name').innerHTML = bodies[i].name;
               document.getElementById('latinName').innerHTML = bodies[i].latinName;
               document.getElementById('desc').innerHTML = bodies[i].desc;
+              document.getElementById('circum').innerHTML = bodies[i].circumference;
+              document.getElementById('circum-title').innerHTML = "OMKRETS";
               document.getElementById('distance').innerHTML = bodies[i].distance;
-              document.getElementById('rotation').innerHTML = bodies[i].rotation;
-          }else{
-            console.log("Planet not found");
+              document.getElementById('dist-title').innerHTML = "KM FRÅN SOLEN";
+              document.getElementById('max-temp').innerHTML = bodies[i].temp.day;
+              document.getElementById('max-temp-title').innerHTML = "MAX TEMPERATURE";
+              document.getElementById('min-temp').innerHTML = bodies[i].temp.day;
+              document.getElementById('min-temp-title').innerHTML = "MIN TEMPERATURE";
           }
 
       }
+      if(planetFound==false)
+      console.log("Planet not found");
   }
   //getPlanetDescription('Solen');
